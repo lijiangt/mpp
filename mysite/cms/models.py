fb7f1f21838699fd15088227f7c1c3bb7c58d39b
lijiangt <lijiangt@gmail.com>
2011-02-21 15:52:46 +0800

    修改模型

--- mysite/cms/models.py
+++ mysite/cms/models.py
@@ -44,7 +44,10 @@ class Category(models.Model):
         return self.seqNum
     
 def get_category_nex_seq_num():
-    return Category.objects.all().aggregate(Max('seqNum')).get("seqNum__max",40) +10
+    maxNum = Category.objects.all().aggregate(Max('seqNum')).get("seqNum__max",40)
+    if maxNum:
+        return maxNum+10
+    return 50
 
 def get_category_by_tag(page):
     return Category.objects.filter(tags__contains=' %s,'%page).order_by('-seqNum')
--- mysite/feedback/models.py
+++ mysite/feedback/models.py
@@ -1,10 +1,29 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
 class Feedback(models.Model):
-#    read
-    pass
-
+    email=models.EmailField(_('Email'),blank=True, null=True)
+    referer=models.URLField(_('Referer'),max_length=255,blank=True, null=True,editable=False)
+    content=models.TextField(_('Content'))
+    read=models.BooleanField(_('Aleady Read'),editable=False,default=False)
+    commited=models.DateTimeField(_('Commit Time'),auto_now_add=True)
+    commitedIp=models.IPAddressField(_('Commit Ip'),editable=False)
+    class Meta:
+        verbose_name = _('Feedback')
+        verbose_name_plural = _('Feedback')
+    def __unicode__(self):
+        return self.id
+            
 class SuggestFeature(models.Model):
-#    read
-    pass
+    name=models.CharField(_('Your Name'),blank=True, null=True,max_length=50)
+    email=models.EmailField(_('Email'),blank=True, null=True)
+    title = models.CharField(_('Title'),max_length=255)
+    content = models.TextField(_('Content'))
+    read=models.BooleanField(_('Aleady Read'),editable=False,default=False)
+    commited=models.DateTimeField(_('Commit Time'),auto_now_add=True)
+    commitedIp=models.IPAddressField(_('Commit Ip'),editable=False)
+    class Meta:
+        verbose_name = _('Feature Suggest')
+        verbose_name_plural = _('Feature Suggest')
+    def __unicode__(self):
+        return self.id
