08a928101100835ec12e751381d155c58b505517
lijiangt <lijiangt@gmail.com>
2011-03-07 21:53:21 +0800

    添加xheditor

--- mysite/cms/views_adm.py
+++ mysite/cms/views_adm.py
@@ -137,10 +137,12 @@ def category_detail(request,id):
     raise Http404
 
 class NormalArticleForm(forms.ModelForm):
+    content=forms.CharField(widget=forms.Textarea(attrs={'class':'xheditor'}))
     class Meta:
         model = Article
         exclude = ('url',)
 class PictureArticleForm(forms.ModelForm):
+    content=forms.CharField(widget=forms.Textarea(attrs={'class':'xheditor'}))
     pic = forms.ImageField(required=True,label=_('Picture'))
     class Meta:
         model = Article
