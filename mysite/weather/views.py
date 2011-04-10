566fd76bcab3fb72ae878f52e3257d444db46c55
lijiangt <lijiangt@gmail.com>
2011-04-10 13:33:02 +0800

    修正错误，为天气预报功能添加缓存

--- mysite/weather/views.py
+++ mysite/weather/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-
+from django.views.decorators.cache import cache_page
 import feedparser
 
 weather_rss_url= 'http://weather.raychou.com/?/detail/54399/rss'
 
+@cache_page(60 * 60)
 def view(request):
     feed = feedparser.parse(weather_rss_url)
     return render_to_response('weather/view.html',{
