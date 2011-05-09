5baaabff40ac3c193f63f611393746a6d32c8642
lijiangt <lijiangt@gmail.com>
2011-05-09 22:23:36 +0800

    修改天气预报实现

--- mysite/weather/views.py
+++ mysite/weather/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.views.decorators.cache import cache_page
-import feedparser
+import pywapi
 
 weather_rss_url= 'http://weather.raychou.com/?/detail/54399/rss'
 
 @cache_page(60 * 60)
 def view(request):
-    feed = feedparser.parse(weather_rss_url)
+    result = pywapi.get_weather_from_google('beijing',hl='zh_CN')
+#    feed = feedparser.parse(weather_rss_url)
     return render_to_response('weather/view.html',{
-                'feed':feed,
-                'entries':feed.entries,
+                'result':result,
                 },context_instance=RequestContext(request))
