14fa092c49730a436f18b5c701c27a793ab27876
lijiangt <lijiangt@gmail.com>
2011-05-10 19:37:56 +0800

    基本完成天气预报功能完善，但需下载各种天气状态的图标

--- mysite/weather/views.py
+++ mysite/weather/views.py
@@ -5,7 +5,7 @@ import pywapi
 
 weather_rss_url= 'http://weather.raychou.com/?/detail/54399/rss'
 
-@cache_page(60 * 60)
+#@cache_page(60 * 60)
 def view(request):
     result = pywapi.get_weather_from_google('beijing',hl='zh_CN')
 #    feed = feedparser.parse(weather_rss_url)
