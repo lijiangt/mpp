aeeda5ea63e575c62eefc2c7705c98ada4d8e733
lijiangt <lijiangt@gmail.com>
2011-05-11 21:51:08 +0800

    完善天气预报

--- mysite/weather/views.py
+++ mysite/weather/views.py
@@ -3,9 +3,9 @@ from django.template import RequestContext
 from django.views.decorators.cache import cache_page
 import pywapi
 
-weather_rss_url= 'http://weather.raychou.com/?/detail/54399/rss'
+#weather_rss_url= 'http://weather.raychou.com/?/detail/54399/rss'
 
-#@cache_page(60 * 60)
+@cache_page(60 * 60)
 def view(request):
     result = pywapi.get_weather_from_google('beijing',hl='zh_CN')
 #    feed = feedparser.parse(weather_rss_url)
