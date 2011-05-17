cca30305c0898d6001688119201218d3b8c88d48
lijiangt <lijiangt@gmail.com>
2011-05-17 21:48:44 +0800

    try wurfl

--- mysite/views.py
+++ mysite/views.py
@@ -8,6 +8,13 @@ def index(request):
     if not page:
         page = settings.PAGES.get('page-home-cn',None)
     apps = page.getApps()
+    user_agent = request.META.get('HTTP_USER_AGENT',None) 
+    from wurfl import devices
+    from pywurfl.algorithms import TwoStepAnalysis
+    search_algorithm = TwoStepAnalysis(devices)
+    device = devices.select_ua(unicode(user_agent), search=search_algorithm)
+    print 'user_agent: %s   device: %s'%(user_agent,device.brand_name)
+    print device.model_name
     return render_to_response('index.html', {
                 'page':page,
                 'apps':apps,
